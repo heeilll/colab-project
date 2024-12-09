72c6a50f29ce42271baab6cd98c0b87544885804
heeilll <heeil1234@naver.com>
2024-12-09 23:00:36 +0900

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
-fetch('data.json') // Colab에서 다운로드한 JSON 파일
-    .then(response => response.json())
-    .then(data => {
-        document.body.innerHTML += `<pre>${JSON.stringify(data, null, 2)}</pre>`;
-    });
+document.getElementById('dashboard').innerHTML = "<p>Dashboard is under construction!</p>";
