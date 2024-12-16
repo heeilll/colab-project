73d60a5242f745b5e64b1f40f7e4806e6818b30c
heeilll <heeil1234@naver.com>
2024-12-16 19:45:07 +0900

    main.py

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,17 +1,15 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 
 app = FastAPI()
 
-# 정적 파일 제공 설정 (dashboard.html 위치)
+# 정적 파일 제공 설정
 app.mount("/static", StaticFiles(directory="docs"), name="static")
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/")
 async def read_root():
-    # index.html을 기본 페이지로 보여줍니다.
-    return HTMLResponse(content=open("docs/index.html", encoding="utf-8").read())
+    return {"message": "대시보드 준비 완료!"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
